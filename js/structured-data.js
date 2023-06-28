0684922c33a96f8ec9c4f7017028e80f4220d7d8
Sahan Sandaruwan <84913793+sahansandaruwan@users.noreply.github.com>
2023-06-29 00:47:09 +0530

    Add files via upload

---
+++ js/structured-data.js
@@ -0,0 +1,16 @@
+{
+    "@context": "http://schema.org",
+    "@type": "NewsWebsite",
+    "name": "Futurehex - Technology and Science News",
+    "description": "Futurehex is a technology and science news website that provides up-to-date information on the latest trends and developments in these fields. Stay informed about future technology, artificial intelligence, robotics, and more.",
+    "url": "https://futurehex.buzz/",
+    "publisher": {
+      "@type": "Organization",
+      "name": "Futurehex",
+      "logo": {
+        "@type": "ImageObject",
+        "url": "https://futurehex.buzz/assets/logo/dark%20logo.png"
+      }
+    }
+  }
+  
